4d412d2ef707a066d6fe56b0782c776e9b17cb4d
gurkenjaeger13 <abegglinus@gmail.com>
2023-03-08 10:00:12 +0100

    added COOKIE (light/dark mode)
    User choice will now be saved as cookie

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
-//darkmode
+//Darkmode Setup
 let lightMode = true;
-let modeElements = [];
-modeElements.push();
-modeElements.push();
-console.log
+//Cookies Setup
+console.log("THE COOKIE IS:",document.cookie);
 
+//cookie for first-time-user
+if ( document.cookie == "" ){
+    document.cookie = "lightMode=true";
+    console.log("user had no cookie");
+}
 
+//if cookie set: change local variable to cookie
+if ( document.cookie == "lightMode=true"){
+    console.log("User had lightmode");
+}
+if ( document.cookie == "lightMode=false"){
+    console.log("User had darkmode");
+    //set website to dark
+    lightMode = false;
+    document.cookie = "lightMode=false"; 
+    console.log("Dark triggered");
+    (document.getElementById("inner")).classList.add("darkmode-inner");
+    (document.getElementById("body")).classList.add("darkmode-body");
+    (document.getElementById("mode-toggle")).classList.add("darkmode-fontcolor-forced");
+    (document.getElementById("mode-toggle")).classList.add("darkmode-button-hover");
+    let buttons = document.getElementsByClassName("button");
+    for (let i=0; i < (document.getElementsByClassName("button")).length; i++ ){
+        buttons[i].classList.add("darkmode-fontcolor-forced");
+        buttons[i].classList.add("darkmode-button-hover");}
+}
+
+//Darkmode Function
 function toggleMode(){
-    
+    console.log("MODE CHANGE TOGGLED");
     if (lightMode) {
         lightMode = false;
+        document.cookie = "lightMode=false"; 
         console.log("Dark triggered");
         (document.getElementById("inner")).classList.add("darkmode-inner");
         (document.getElementById("body")).classList.add("darkmode-body");
@@ -24,6 +49,7 @@ function toggleMode(){
 
     else{
         lightMode = true;
+        document.cookie = "lightMode=true";
         console.log("Light triggered");
         (document.getElementById("inner")).classList.remove("darkmode-inner");
         (document.getElementById("body")).classList.remove("darkmode-body");
@@ -37,5 +63,16 @@ function toggleMode(){
     }
 
 
+}
+
+
+//Cookie
+function getCookie(){
+    document.cookie
+    .split(';')
+    .map(cookie => cookie.split('='))
+    .reduce((accumulator, [key,value]) =>
+        ({...accumulator, [key.trim()]: decodeURIComponent(value)}),
+        {});
 }
 
