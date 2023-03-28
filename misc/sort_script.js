34363ee24cd80743a1020bf4d9e73e991d33feac
freshefisch <abegglinus@gmail.com>
2023-03-28 20:11:19 +0200

    smaller changes
    spelling, styling, html build, etc.

--- misc/sort_script.js
+++ misc/sort_script.js
@@ -10,13 +10,8 @@ function pasteRand(){
     // uses the Fisher–Yates shuffle
     sampleRand = [...sampleBest];
     let m = sampleRand.length, t, i;
-    // While there remain elements to shuffle…
     while (m) {
-  
-      // Pick a remaining element…
       i = Math.floor(Math.random() * m--);
-  
-      // And swap it with the current element.
       t = sampleRand[m];
       sampleRand[m] = sampleRand[i];
       sampleRand[i] = t;
